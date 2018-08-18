1a6cae079fd0985d67e7981280a30d642d948d86
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-08-18 10:26:50 +0800

    First attempt at using rollup with jest

--- test/puppeteer.test.js
+++ test/puppeteer.test.js
@@ -11,28 +11,28 @@ function example (what) {
 describe('puppeteer tests', () => {
   let page
 
-  it('basic.html', async () => {
-    const url = example('basic.html')
+  // it('basic.html', async () => {
+  //   const url = example('basic.html')
 
-    page = await browser.newPage()
-    await page.goto(url)
-    await page.waitForSelector('#greeter')
-    const text = await getContent(page)
-    expect(text).toContain('Oh hello, John!')
-  })
+  //   page = await browser.newPage()
+  //   await page.goto(url)
+  //   await page.waitForSelector('#greeter')
+  //   const text = await getContent(page)
+  //   expect(text).toContain('Oh hello, John!')
+  // })
 
-  it('es5.html', async () => {
-    const url = example('es5.html')
+  // it('es5.html', async () => {
+  //   const url = example('es5.html')
 
-    page = await browser.newPage()
-    await page.goto(url)
-    await page.waitForSelector('#greeter')
-    const text = await getContent(page)
-    expect(text).toContain('Oh hello, John!')
-  })
+  //   page = await browser.newPage()
+  //   await page.goto(url)
+  //   await page.waitForSelector('#greeter')
+  //   const text = await getContent(page)
+  //   expect(text).toContain('Oh hello, John!')
+  // })
 
   it('mocha tests', async () => {
-    const url = example('../dist/index.html')
+    const url = 'http://localhost:10049/'
     page = await browser.newPage()
     await page.goto(url)
     await page.waitForSelector('#finish')

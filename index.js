b47b16450e21e33f2994483274e748f64e747042
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-08-19 16:55:57 +0800

    Move custom elements logic into lib/custom_elements

--- index.js
+++ index.js
@@ -2,29 +2,16 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 
+import { defineOne } from './lib/custom_elements'
+
 /*::
-export type Component = React.ComponentType<{}>
-
-export type PropertyMap = {
-  [string]: ?string
-}
-
-export type ElementMap = {
-  [string]: ElementSpec | Component
-}
-
-export type Defaults = {
-  attributes?: Array<string>,
-  quiet?: boolean,
-  shadow?: boolean
-}
-
-export type ElementSpec = {
-  component: Component,
-  attributes?: Array<string>,
-  quiet?: boolean,
-  shadow?: boolean
-}
+import type {
+  Component,
+  PropertyMap,
+  ElementMap,
+  Defaults,
+  ElementSpec
+} from './lib/types'
 */
 
 /**
@@ -37,7 +24,8 @@ export function define (
 ) {
   Object.keys(components).forEach((name /*: string */) => {
     const elSpec /*: ElementSpec */ = toElementSpec(components[name])
-    defineOne(Object.assign({}, defaults, elSpec), name)
+    const newElSpec = Object.assign({}, defaults, elSpec)
+    defineOne(newElSpec, name, update, unmount)
   })
 }
 
@@ -50,84 +38,6 @@ function toElementSpec (
 }
 
 /**
- * Registers one element.
- * @private
- */
-
-function defineOne (elSpec /*: ElementSpec */, name /*: string */) {
-  const attributes = elSpec.attributes || []
-
-  class ComponentElement extends window.HTMLElement {
-    static get observedAttributes () {
-      return ['props-json', ...attributes]
-    }
-
-    connectedCallback () {
-      this._mountPoint = createMountPoint(this, elSpec)
-      update(this, elSpec, this._mountPoint)
-    }
-
-    disconnectedCallback () {
-      if (!this._mountPoint) return
-      ReactDOM.unmountComponentAtNode(this._mountPoint)
-    }
-
-    attributeChangedCallback () {
-      if (!this._mountPoint) return
-      update(this, elSpec, this._mountPoint)
-    }
-  }
-
-  if (!ensureSupported()) return
-
-  // Supress warning when quiet mode is on
-  if (elSpec.quiet && window.customElements.get(name)) {
-    return
-  }
-
-  window.customElements.define(name, ComponentElement)
-}
-
-/**
- * Ensures that custom elements are supported
- * @private
- */
-
-function ensureSupported () {
-  if (!window.customElements || !window.customElements.define) {
-    console.error(
-      "remount: Custom elements aren't support in this browser. " +
-        'Remount will not work. ' +
-        'Including polyfills will likely fix this. ' +
-        'See Remount documentation for more info: ' +
-        'https://github.com/rstacruz/remount'
-    )
-    return false
-  }
-
-  return true
-}
-
-/**
- * Creates a `<span>` element that serves as the mounting point for React
- * components.
- * @private
- */
-
-function createMountPoint (
-  element /*: Element */,
-  { shadow } /*: ElementSpec */
-) {
-  if (shadow) {
-    const mountPoint = document.createElement('span')
-    element.attachShadow({ mode: 'open' }).appendChild(mountPoint)
-    return mountPoint
-  } else {
-    return element
-  }
-}
-
-/**
  * Updates a custom element by calling `ReactDOM.render()`.
  * @private
  */
@@ -137,13 +47,19 @@ function update (
   { component, attributes } /*: ElementSpec */,
   mountPoint /* Element */
 ) {
-  const props = element.hasAttribute('props-json')
-    ? JSON.parse(element.getAttribute('props-json'))
-    : getProps(element, attributes)
-
+  const rawJson = element.getAttribute('props-json')
+  const props = rawJson ? JSON.parse(rawJson) : getProps(element, attributes)
   const reactElement = React.createElement(component, props)
-
   ReactDOM.render(reactElement, mountPoint)
+}
+
+/**
+ * Unmount
+ * @private
+ */
+
+function unmount (mountPoint /*: Element */) {
+  ReactDOM.unmountComponentAtNode(mountPoint)
 }
 
 /**

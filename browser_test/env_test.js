4af6fbd8f2d04843be0f5a8c0f32c5137ed5d380
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-08-20 01:18:26 +0800

    Show in tests if MutationObserver was polyfilled

--- browser_test/env_test.js
+++ browser_test/env_test.js
@@ -4,7 +4,13 @@ import { Remount } from './setup'
 describe('Remount mode: ' + Remount.adapterName, () => {
   if (Remount.adapterName === 'MutationObserver') {
     it('Custom Elements are not supported on this platform.')
-    it('Falling back to MutationObserver.')
+
+    const ms = window.MutationObserver._period
+    if (ms) {
+      it(`Falling back to MutationObserver (polyfill, polling ${ms}ms).`)
+    } else {
+      it('Falling back to MutationObserver (native).')
+    }
   } else if (Remount.adapterName === 'CustomElements') {
     it('Custom Elements are supported!')
     it(':)')

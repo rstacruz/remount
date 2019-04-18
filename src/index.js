869d06eb6f47526a28fda8f7ea63d448a1819cf8
Ryan Bonte <ryan.bonte@gmail.com>
2019-04-18 16:52:24 +0200

    retarget

--- src/index.js
+++ src/index.js
@@ -82,7 +82,7 @@ export function define(components, defaults) {
     Strategy.defineElement(elSpec, name, {
       onMount(element, mountPoint) {
         const props = getProps(element, elSpec.attributes)
-        adapter.mount(elSpec, mountPoint, props)
+        adapter.mount(elSpec, mountPoint, props, element)
       },
 
       onUpdate(element, mountPoint) {
